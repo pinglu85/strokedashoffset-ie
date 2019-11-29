08c35d3526546058e1a7e5e23f29bc02a62bbc4d
pinglu85 <luping8576@hotmail.com>
2019-11-29 01:16:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const isIE = /*@cc_on!@*/ false || !!document.documentMode;
+
+const AnimateSVG = styled.svg`
+  & .animate {
+    stroke-dashoffset: 1000;
+    animation: fadeInP 10s linear infinite;
+  }
+  @keyframes fadeInP {
+    to {
+      stroke-dashoffset: 0;
+    }
+  }
+  & .path {
+    stroke-dashoffset: ${props => props.offset};
+  }
+`;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { offset: 1000 };
+  }
+
+  componentDidMount() {
+    if (isIE) {
+      this.offsetMe();
+    }
+  }
+
+  offsetMe = () => {
+    if (this.state.offset < 0) {
+      this.setState({ offset: 1000 });
+    }
+    const newOffset = this.state.offset - 1;
+    this.setState({ offset: newOffset });
+    requestAnimationFrame(this.offsetMe);
+  };
+
+  render() {
+    let text = null;
+    let className = 'animate';
+    if (isIE) {
+      text = <p>Browser is IE</p>;
+      className = 'path';
+    } else {
+      text = <p>Browser is not IE</p>;
+      className = 'animate';
+    }
+    return (
+      <div>
+        {text}
+        <AnimateSVG offset={this.state.offset} viewBox="0 0 400 400">
+          <path
+            fill="none"
+            stroke="#000"
+            strokeDasharray="10 10"
+            strokeWidth="8"
+            d="M0 0c100 10 200 80 300 15"
+            className={className}
+          ></path>
+        </AnimateSVG>
+      </div>
+    );
+  }
 }
 
 export default App;
